3e7058c1bc1b6e3b35b26ad4518a077a0ae95414
ngduyanhece <ngduyanhece@gmail.com>
2020-01-14 12:27:40 +0700

    add ner

---
+++ eval_ner.py
@@ -0,0 +1,53 @@
+from fairseq import checkpoint_utils, data, options, tasks
+import os
+from sklearn.metrics import f1_score
+# Parse command-line arguments for generation
+parser = options.get_generation_parser(default_task='translation')
+args = options.parse_args_and_arch(parser)
+
+# Setup task
+task = tasks.setup_task(args)
+
+# Load model
+print('| loading model from {}'.format(args.path))
+models, _model_args = checkpoint_utils.load_model_ensemble([args.path], task=task)
+model = models[0]
+
+### open file and read data
+testdata = []
+labels = []
+predictions = []
+with open(os.path.join('bio','test.in')) as infile:  # using `with open()` you do not need to do `infile.close()`, that is done for you.
+    for line in infile:  # I
+        testdata.append(line.strip())
+
+with open(os.path.join('bio','test.bio')) as infile:  # using `with open()` you do not need to do `infile.close()`, that is done for you.
+    for line in infile:  # I
+        labels.append(line.strip())
+
+for d,l in zip(testdata,labels):
+
+    # Tokenize into characters
+    tokens = task.source_dictionary.encode_line(
+        d, add_if_not_exist=False,
+    )
+
+    # Build mini-batch to feed to the model
+    batch = data.language_pair_dataset.collate(
+        samples=[{'id': -1, 'source': tokens}],  # bsz = 1
+        pad_idx=task.source_dictionary.pad(),
+        eos_idx=task.source_dictionary.eos(),
+        left_pad_source=False,
+        input_feeding=False,
+    )
+
+    # Feed batch to the model and get predictions
+    preds = model(**batch['net_input'])
+    top_scores, top_labels = preds[0].topk(k=1)
+    # label_name = task.target_dictionary.string([top_labels[0]])
+    # predictions.append(label_name)
+    print(top_labels)    
+
+# # calculate the metric 
+# f1 = f1_score(labels, predictions, average='weighted')
+# print("F1 score: {}".format(f1))
